29783070aa3a2086ccf049a06956ea01b660c5c3
Shrikant Yadav <shrikant.webllisto@gmail.com>
2019-07-08 15:09:53 +0530

    card fix

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -11,6 +11,7 @@ class Card extends Component {
     state = { 
         testidata: [
             {
+                id: '1',
                 person: 'Shrikant Yadav',
                 company: 'Company Name',
                 imgsrc: 'https://shrikant9907.github.io/webstyle/images/user_logo.png',
@@ -18,22 +19,17 @@ class Card extends Component {
                 text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
             },
             {
+                id: '2',
                 person: 'Shrikant Yadav',
                 company: 'Company Name',
                 imgsrc: 'https://shrikant9907.github.io/webstyle/images/user_logo.png',
                 imgalt: 'Shrikant Image',
                 text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-            },
-            {
-                person: 'Shrikant Yadav',
-                company: 'Company Name',
-                imgsrc: 'https://shrikant9907.github.io/webstyle/images/user_logo.png',
-                imgalt: 'Shrikant Image',
-                text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-            }
+            } 
         ],
         basicdata: [
             {
+                id: '1',
                 title: 'Card Title',
                 imgsrc: 'https://picsum.photos/id/891/300/200',
                 imgalt: 'Card Image',
@@ -41,20 +37,7 @@ class Card extends Component {
                 text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
             },
             {
-                title: 'Company Name',
-                imgsrc: 'https://picsum.photos/id/891/300/200',
-                imgalt: 'Shrikant Image',
-                showimg: true,
-                text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-            },
-            {
-                title: 'Company Name',
-                imgsrc: 'https://picsum.photos/id/891/300/200',
-                imgalt: 'Shrikant Image',
-                showimg: true,
-                text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-            },
-            {
+                id: '2',
                 title: 'Company Name',
                 imgsrc: 'https://picsum.photos/id/891/300/200',
                 imgalt: 'Shrikant Image',
@@ -65,56 +48,36 @@ class Card extends Component {
         ],
         iconCarddata: [
             {
+                id: '1',
                 title: 'Card Title',
                 icon: <i className="fas fa-cloud"></i>,
                 iconLocation: 'right',
                 text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
             },
             {
+                id: '2',
                 title: 'Company Name',
                 icon: <i className="fas fa-cloud"></i>,
                 iconLocation: 'right',
                 text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-            },
-            {
-                title: 'Company Name',
-                icon: <i className="fas fa-cloud"></i>,
-                iconLocation: 'left',
-                text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-            },
-            {
-                title: 'Company Name',
-                icon: <i className="fas fa-cloud"></i>,
-                iconLocation: 'left',
-                text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-            }
+            } 
              
         ],
         pricingCarddata: [
             {
+                id: '1',
                 title: 'Card Title',
                 icon: <i className="fas fa-cloud"></i>,
                 iconLocation: 'right',
                 text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
             },
             {
+                id: '2',
                 title: 'Company Name',
                 icon: <i className="fas fa-cloud"></i>,
                 iconLocation: 'right',
                 text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-            },
-            {
-                title: 'Company Name',
-                icon: <i className="fas fa-cloud"></i>,
-                iconLocation: 'left',
-                text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-            },
-            {
-                title: 'Company Name',
-                icon: <i className="fas fa-cloud"></i>,
-                iconLocation: 'left',
-                text: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
-            }
+            } 
              
         ]
     }  
@@ -123,13 +86,13 @@ class Card extends Component {
 
         let card;
         if (this.props.type==='testimonial') {
-            card =  this.state.testidata.map(sdata => <div className="col-12 col-sm-4"><TestimonialCard data={sdata} key="1" /></div>);
+            card =  this.state.testidata.map(sdata => <div className="col-12 col-sm-4" key={sdata.id}><TestimonialCard data={sdata} key={sdata.id} id={sdata.id} /></div>);
         } else if (this.props.type==='basic') {
-            card =  this.state.basicdata.map(sdata => <div className="col-12 col-sm-6"><BasicCard data={sdata} key="2" /></div>);
+            card =  this.state.basicdata.map(sdata => <div className="col-12 col-sm-6" key={sdata.id}><BasicCard data={sdata} key={sdata.id} id={sdata.id} /></div>);
         } else if (this.props.type==='iconCard') {
-            card =  this.state.iconCarddata.map(sdata => <div className="col-12 col-sm-6"><IconCard data={sdata} key="3" /></div>);
+            card =  this.state.iconCarddata.map(sdata => <div className="col-12 col-sm-6" key={sdata.id}><IconCard data={sdata} key={sdata.id} id={sdata.id} /></div>);
         }else if (this.props.type==='pricingCard') {
-            card =  this.state.pricingCarddata.map(sdata => <div className="col-12 col-sm-3"><PricingCard data={sdata} key="3" /></div>);
+            card =  this.state.pricingCarddata.map(sdata => <div className="col-12 col-sm-3" key={sdata.id}><PricingCard data={sdata} key={sdata.id} id={sdata.id} /></div>);
         }else {
             card = <p>Cart type not given.</p>
         }
