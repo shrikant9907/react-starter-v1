8e71f4107c9407a678a4152a11cb3688cc7b6ea0
Shrikant Yadav <shrikant9907@gmail.com>
2021-11-06 17:49:17 +0530

    basic pages added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React , {Component, Fragment} from 'react'; 
+import React from 'react'; 
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./layouts/home";
+import About from "./layouts/about";
+import Blog from "./layouts/blog";
+import Shop from "./layouts/shop";
+import Contact from "./layouts/contact";
 
-// import Header from './components/header/Header';
-import Content from './components/content/Content';
-// import Footer from './components/footer/Footer';
 
-class App extends Component {
- 
-  render() { 
-    return (
-      <Fragment>
-          {/* <Header brandname="Company Name" /> */}
-          <Content />
-          {/* <Footer /> */}
-      </Fragment>
-    );
-  }
+
+const App = () => {
+  return (
+    <Router>
+      <nav className="navui1">
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/blog'>Blog</Link></li>
+          <li><Link to='/shop'>Shop</Link></li>
+          <li><Link to='/contact'>Contact Us</Link></li>
+        </ul>
+      </nav>
+
+      <div className="pagewr">
+        <Routes>
+          <Route exact path="/" component={ Home } />
+          <Route path="/about" component={ About }/>
+          <Route path="/blog" component={ Blog }/>
+          <Route path="/shop" component={ Shop }/>
+          <Route path="/contact" component={ Contact }/>
+        </Routes>
+      </div>
+
+    </Router>
+  );
 }
 
 export default App;   
---
+++ src/layouts/checkout/index.js
@@ -0,0 +1,23 @@
+import './Home.scss';
+import Banner from '../../components/banner/Banner';
+import Carousel from '../../components/carousel/Carousel'; 
+import UsersListCarousel from '../../components/carousel/usersListCarousel'; 
+  
+const Home = (props) => {
+  return (
+    <>
+      <div className="seperator s20"></div>
+      <section className="app-section">
+        <Banner />
+      </section>
+      <section className="app-section">
+        <UsersListCarousel />
+      </section>
+      <section className="app-section">
+        <Carousel heading="Sample carousel" />
+      </section>
+    </>
+  )
+}
+
+export default Home;
